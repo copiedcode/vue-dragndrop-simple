2502ea4cb6316999b996e76a71dab9f66f6bd25a
copiedcode <raphael.webdesign@outlook.com>
2020-05-04 20:01:45 +0200

    add drag function

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="flexbox">
+      <Board id="board-1">
+        <Card id="card-1" draggable="true">
+          <p>Card 1 - draggable</p>
+        </Card>
+      </Board>
+
+      <Board id="board-2">
+        <Card id="card-2" draggable="true">
+          <p>Card 2 - not draggable</p>
+        </Card>
+        <Card id="card-3" draggable="true">
+          <p>Card 3 - not draggable</p>
+        </Card>
+        <Card id="card-4" draggable="true">
+          <p>Card 4 - draggable</p>
+        </Card>
+      </Board>
+      <Board id="board-3"></Board>
+      <Board id="board-4"></Board>
+    </main>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Board from "./components/Board";
+import Card from "./components/Card";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Board,
+    Card
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color: #eee;
+}
+
+.flexbox{
+  display: flex;
+  justify-content: space-between;
+
+  width: 100%;
+  max-width: 90vw;
+  height: 100vh;
+
+  overflow: hidden;
+  margin: 0 auto;
+  padding: 15px;
+}
+
+.flexbox .board {
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  max-width: 20vw;
+
+  background-color: #888;
+  padding: 15px;
+}
+
+.flexbox .board .card {
+  padding: 15px 25px;
+  background-color: white;
+
+  cursor: pointer;
+  margin-bottom: 15px;
 }
 </style>
